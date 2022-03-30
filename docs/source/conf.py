03845eddea012cf74aea0d445973f41ab7ba5efd
catgloss <97563291+catgloss@users.noreply.github.com>
2022-03-29 21:51:12 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cluster Setup Guide'
+author = 'Vector Institute'
 
 release = '0.1'
 version = '0.1.0'
